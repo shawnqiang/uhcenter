c2f6d4622f77c2b26b7317a22629452c417483c5
shawn qiang <shawnqiang@yahoo.com>
2011-10-23 22:44:03 +0800

    add the original prototype

---
+++ data/sitemap.js
@@ -0,0 +1,20 @@
+﻿var sitemap = 
+{
+"rootNodes":[
+{
+"pageName":"Home",
+"type":"Wireframe",
+"url":"Home.html",
+"children":[
+{
+"pageName":"Category-Landing-Page",
+"type":"Wireframe",
+"url":"Category-Landing-Page.html"},
+{
+"pageName":"Products List",
+"type":"Wireframe",
+"url":"Products_List.html"},
+{
+"pageName":"Product",
+"type":"Wireframe",
+"url":"Product.html"}]}]};
